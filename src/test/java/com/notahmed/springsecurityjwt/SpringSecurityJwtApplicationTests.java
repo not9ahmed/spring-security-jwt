1640ce76be3b5951af08901126a2249dc6d06f27
Ahmed Al-Thawadi <ahmed9althawadi@gmail.com>
2023-10-23 22:42:28 +0300

    first commit local

---
+++ src/test/java/com/notahmed/springsecurityjwt/SpringSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notahmed.springsecurityjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
